f72d7c3a5c2da221b50dc7f6ac8b124fc1e82776
RoryWatts <rory.watts@gmail.com>
2023-05-28 21:17:49 +0800

    returns equity instead of ledger

--- src/runtime.py
+++ src/runtime.py
@@ -1,6 +1,6 @@
 import datetime
 from src.utils import validate_datetime
-from . import streams, utils
+from . import streams, utils, ledger
 
 
 def simulate(configuration):
@@ -22,24 +22,13 @@ def simulate(configuration):
     end_date = configuration["runtime"].get("end_date", datetime.date.today() + datetime.timedelta(days=365))
     end_date = validate_datetime(end_date)
 
-    ledger = streams.ledger_handler(
+    _ledger = streams.ledger_handler(
         configuration,
         start_date,
         end_date
         )
 
-    ledger = utils.clean_handler(ledger)
+    _ledger = utils.clean_handler(_ledger)
+    equity = ledger.calculate_equity(_ledger)
 
-    return ledger
-
-
-if __name__ == "__main__":
-    start_date = "2021-01-01"
-    end_date = "2023-12-31"
-    config = {
-        "runtime": {
-            "start_date": start_date,
-            "end_date": end_date,
-        },
-    }
-    simulate(config)
+    return equity
