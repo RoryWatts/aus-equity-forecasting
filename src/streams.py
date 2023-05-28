7be4e9fdf9c39144585450cd7c3e0331318d1499
RoryWatts <rory.watts@gmail.com>
2023-05-28 21:18:00 +0800

    updated handlers for basic types.

--- src/streams.py
+++ src/streams.py
@@ -25,8 +25,28 @@ def ledger_handler(configuration, start_date, end_date):
         List of transactions.
     """
     ledger = []
-    purchases = configuration.get("purchases", {})
-    ledger += purchases_handler(purchases, start_date, end_date)
+    ledger += assets_handler(
+        configuration.get("assets", []),
+        start_date
+    )
+    ledger += liabilities_handler(
+        configuration.get("liabilities", []),
+        start_date
+    )
+    ledger += purchases_handler(
+        configuration.get("purchases", []),
+        start_date, 
+        end_date)
+    ledger += expenses_handler(
+        configuration.get("expenses", []),
+        start_date,
+        end_date
+    )
+    ledger += revenues_handler(
+        configuration.get("revenues", []),
+        start_date,
+        end_date
+    )
     return ledger
 
 
@@ -39,17 +59,77 @@ def purchases_handler(purchases, start_date, end_date):
     return records
 
 
-def assets_handler(assets):
-    return {}
+def assets_handler(assets, start_date):
+    records = []
+    for asset in assets:
+        records.append(
+            ledger.create_record(
+                description=asset["description"],
+                debit="assets",
+                credit="revenues",
+                amount=asset["amount"],
+                date=start_date
+            )
+        )
+    return records
 
 
-def expenses_handler(expenses):
-    return {}
+def liabilities_handler(liabilities, start_date):
+    records = []
+    for liability in liabilities:
+        records.append(
+            ledger.create_record(
+                description=liability["description"],
+                debit="expenses",
+                credit="liabilities",
+                amount=liability["amount"],
+                date=start_date
+            )
+        )
+    return records
 
 
-def liabilities_handler(liabilities):
-    return {}
+def expenses_handler(expenses, start_date, end_date):
+    records = []
+    for expense in expenses:
+        if expense.get("ends", None):
+            end_date = utils.validate_datetime(expense["ends"])
+        dates = utils.calculate_dates(
+            start_date,
+            expense["period"],
+            end_date
+        )
+        for date in dates:
+            records.append(
+                ledger.create_record(
+                    description=expense["description"],
+                    debit="expenses",
+                    credit="assets",
+                    amount=expense["amount"],
+                    date=date
+                    )
+                )
+    return records
 
 
-def revenues_handler(revenues):
-    return {}
+def revenues_handler(revenues, start_date, end_date):
+    records = []
+    for revenue in revenues:
+        if revenue.get("ends", None):
+            end_date = utils.validate_datetime(revenue["ends"])
+        dates = utils.calculate_dates(
+            start_date,
+            revenue["period"],
+            end_date
+        )
+        for date in dates:
+            records.append(
+                ledger.create_record(
+                    description=revenue["description"],
+                    debit="assets",
+                    credit="revenues",
+                    amount=revenue["amount"],
+                    date=date
+                    )
+                )
+    return records
